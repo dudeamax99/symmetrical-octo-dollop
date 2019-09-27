b950e8836036cc6e07e0a84d7e9faba2844c26c5
Dudeamax99 <Dudeamax99@yahoo.com>
2019-09-27 09:37:50 -0400

    Fixed build error Was looking for "contents.rst", main file changed to "index.rst"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+#Sets the master file
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
